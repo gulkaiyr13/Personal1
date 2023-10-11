ba6c14f2aa6a4bf8f3ab4796c7424e4c6362c272
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-12 03:38:59 +0600

    PersonalAccount.java

--- src/PersonalAccount.java
+++ src/PersonalAccount.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class PersonalAccount {
+import java.util.ArrayList;
+
+public class PersonalAccount {
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+    private ArrayList<Amount> transactions;
+
+    public PersonalAccount(int accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            transactions.add(new Amount(amount, "Deposit"));
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            transactions.add(new Amount(amount, "Withdrawal"));
+        }
+    }
+
+    public void printTransactionHistory() {
+        for (Amount transaction : transactions) {
+            System.out.println("Transaction Type: " + transaction.getTransactionType() +
+                    ", Amount: " + transaction.getAmount());
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
 }
