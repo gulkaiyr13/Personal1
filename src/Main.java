a178c44d32d1a95d0cc8ec90e5bb25dd6ad0373a
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-12 03:38:35 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PersonalAccount account = new PersonalAccount(123456, "John Doe");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        account.deposit(1000.0);
+        account.withdraw(500.0);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Transaction History for " + account.getAccountHolder());
+        account.printTransactionHistory();
+
+        System.out.println("Account Number: " + account.getAccountNumber());
+        System.out.println("Account Holder: " + account.getAccountHolder());
+        System.out.println("Current Balance: " + account.getBalance());
     }
 }
